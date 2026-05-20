801ed7fb778acb687bb1e72b8c898d1228f74399
anuhassithijha-tech <anuhassithijha@gmail.com>
2026-05-20 20:06:15 +0530

    Create alive.js

---
+++ plugins/alive.js
@@ -0,0 +1,81 @@
+const { cmd } = require('../command');
+const config = require('../config');
+const os = require("os");
+
+cmd({
+    pattern: "alive",
+    desc: "Check Bot Status",
+    category: "main",
+    filename: __filename
+},
+async (danuwamd, mek, m, {
+    from,
+    reply
+}) => {
+
+    try {
+
+        const uptime = process.uptime();
+        const ping = Date.now() - mek.messageTimestamp * 1000;
+        const ram = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2);
+
+        let aliveMsg = `
+🛡┃ *S I T H I J A - M D* | v1.0 ┃🛡
+
+🌿 👋 *SYSTEM ONLINE!*
+*YOUR BOT IS RUNNING PERFECTLY*
+
+╭───────────────❍
+│ *S Y S T E M  S T A T S*
+│
+│ 📗 *STATUS* : ONLINE
+│ 💻 *VERSION* : 1.0.0
+│ 🛡️ *MODE* : PUBLIC
+│ ⚡ *LATENCY* : ${ping}ms
+│ ⏳ *UPTIME* : ${runtime(uptime)}
+╰───────────────❍
+
+╭───────────────❍
+│ *O W N E R  C O N T E X T*
+│
+│ 👤 *OWNER* : SITHIJA
+│ 🌿 *LIBRARY* : BAILEYS
+│ 📍 *LOCATION* : SRI LANKA
+╰───────────────❍
+
+╭───────────────❍
+│ *S E R V E R  I N F O*
+│
+│ 🍀 *RAM* : ${ram}MB
+│ 🪴 *NODE* : ${process.version}
+│ 💻 *PLATFORM* : ${os.platform()}
+╰───────────────❍
+
+✅ Use *.menu* to access commands
+✅ Use *.owner* for support
+
+> ⚡ POWERED BY SITHIJA-MD
+`;
+
+        await danuwamd.sendMessage(from, {
+            image: { url: config.ALIVE_IMG },
+            caption: aliveMsg
+        }, { quoted: mek });
+
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
+
+function runtime(seconds) {
+
+    seconds = Number(seconds);
+
+    const d = Math.floor(seconds / (3600 * 24));
+    const h = Math.floor(seconds % (3600 * 24) / 3600);
+    const m = Math.floor(seconds % 3600 / 60);
+    const s = Math.floor(seconds % 60);
+
+    return `${d}d ${h}h ${m}m ${s}s`;
+}
