9b26848e03957793e2fc1f070923d2aa061e725e
anuhassithijha-tech <anuhassithijha@gmail.com>
2026-05-20 20:12:25 +0530

    Create owner.js

---
+++ plugins/owner.js
@@ -0,0 +1,38 @@
+const { cmd } = require("../command");
+
+cmd({
+    pattern: "owner",
+    desc: "Owner Contact",
+    category: "owner",
+    react: "👑",
+    filename: __filename
+},
+async (danuwa, mek, m, {
+    from,
+    reply
+}) => {
+
+try {
+
+await danuwa.sendMessage(
+from,
+{
+contacts: {
+displayName: "BOT OWNER Sithija Anuhas 👑 ",
+contacts: [{
+vcard: `BEGIN:VCARD
+VERSION:3.0
+FN:BOT OWNER:-MR.Sithija Anuhas 👑
+TEL;type=CELL;type=VOICE;waid=94785936039:+94785936039
+END:VCARD`
+}]
+}
+},
+{ quoted: mek }
+);
+
+} catch (e) {
+console.log(e);
+reply("❌ Owner contact send failed");
+}
+});
