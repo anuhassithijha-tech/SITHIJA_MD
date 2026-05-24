a12644416836ed0b3cbfaf0e5267c4f471267877
anuhassithijha-tech <anuhassithijha@gmail.com>
2026-05-25 01:22:59 +0530

    Update minitest.js

--- plugins/minitest.js
+++ plugins/minitest.js
@@ -1,16 +1,32 @@
 const { cmd } = require("../command");
 
 cmd({
-pattern: "test2",
-category: "test"
+pattern: "test",
+react: "✅",
+desc: "Button Test",
+category: "test",
+filename: __filename
 },
 async (conn, mek, m, { from }) => {
 
-await conn.sendMessage(from,{
-text:
-"🌸 BUTTON TEST 🌸\n\nReply .ping"
-},{
+await conn.sendMessage(
+from,
+{
+text: "🌸 BUTTON WORKING",
+footer: "SITHIJA MD",
+templateButtons: [
+{
+index: 1,
+quickReplyButton: {
+displayText: "PING",
+id: ".ping"
+}
+}
+]
+},
+{
 quoted: mek
-});
+}
+);
 
 });
