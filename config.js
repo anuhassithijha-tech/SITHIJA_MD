a0504f6554d382809de931b42403e647a75e2e59
anuhassithijha-tech <anuhassithijha@gmail.com>
2026-05-25 00:15:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "lAcWFR7Y#Fn2cGZfcIjsU6QfTopeXeUMHxkUii64J-wOoTki10wc",
+SESSION_ID: process.env.SESSION_ID || "8ZVCgJgK#5NiMQ2NxzFYsc94wd514A9K3YFR88uPsFXuqBv3pU0w",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sithija-bot/SITHIJA_MD/blob/main/alive.png1.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*⚡ SITHIJA MD Alive❤️ Fast & Active Always*",
+ALIVE_MSG: process.env.ALIVE_MSG || "*⚡ SITHIJA MD MOVIE BOT Alive❤️ Fast & Active Always*",
 BOT_OWNER: '94785936039',  // Replace with the owner's phone number
 
 
