a0682b3902cee4b438d8f61b003f679ce8e308ff
anuhassithijha-tech <anuhassithijha@gmail.com>
2026-05-25 00:53:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "8ZVCgJgK#5NiMQ2NxzFYsc94wd514A9K3YFR88uPsFXuqBv3pU0w",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sithija-bot/SITHIJA_MD/blob/main/alive.png1.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*⚡ SITHIJA MD MOVIE BOT Alive❤️ Fast & Active Always*",
+MENU_IMAGE: process.env.MENU_IMAGE || "https://github.com/sithija-bot/SITHIJA_MD/blob/main/alive.png1.png?raw=true",
 BOT_OWNER: '94785936039',  // Replace with the owner's phone number
 
 
